c1998474ff4064456b6981a27a41f1afa2f72ae0
david misheal <davidmichealll67@gmail.com>
2024-06-22 21:00:40 +0300

    profile updates - favourtie places added

--- Node/routes/userRoutes.js
+++ Node/routes/userRoutes.js
@@ -27,6 +27,11 @@ router.delete('/deleteMe', userController.deleteMe);
 //router.post('/me/photo', userController.uploadImage); // Ensure this line is correct
 //router.get('/:userId/photo', userController.getImage);
 
+router.get('/favorites', userController.getUserFavorites);
+router.post('/favorites', userController.addUserFavorite);
+
+router.delete('/favorites', userController.removeFavorite); // Add this line
+
 router.use(authController.restrictTo('admin'));
 
 router
